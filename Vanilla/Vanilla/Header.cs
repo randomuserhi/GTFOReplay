7670dc3b1fceda016f45975e6be4049d3a913ad9
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-06-14 18:47:15 +0100

    Update Header.cs

--- Vanilla/Vanilla/Header.cs
+++ Vanilla/Vanilla/Header.cs
@@ -5,7 +5,7 @@ using ReplayRecorder.API.Attributes;
 namespace Vanilla.Metadata {
     [ReplayData("Vanilla.Metadata", "0.0.1")]
     internal class Metadata : ReplayHeader {
-        const string Version = "0.0.5";
+        const string Version = "0.0.6";
 
         [ReplayInit]
         private static void Init() {
