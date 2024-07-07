61cce35343ac233428f091abfe88183f6c1bbf65
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-07-07 05:08:16 +0100

    Update enemy.ts

--- Viewer/assets/src/modules/parser/datablocks/enemy.ts
+++ Viewer/assets/src/modules/parser/datablocks/enemy.ts
@@ -1,5 +1,5 @@
-import { EnemyDatablock } from "../../../datablocks/enemy/enemy";
-import { Identifier } from "../../identifier";
+import { EnemyDatablock } from "../../datablocks/enemy/enemy";
+import { Identifier } from "../identifier";
 
 EnemyDatablock.set(Identifier.create("Enemy", 0), {
     name: "Unknown",
