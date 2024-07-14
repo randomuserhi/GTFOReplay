7d8367e5555d32ad19b1fc3fad6ee01753dadd63
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-07-14 09:27:49 +0100

    Update info.ts

--- Viewer/assets/src/profiles/vanilla/ui/pages/info.ts
+++ Viewer/assets/src/profiles/vanilla/ui/pages/info.ts
@@ -26,7 +26,7 @@ versionInfo.set("0.0.8", `- Mines now use a proper Identifier for modded support
 - Thrown items are now shown
 - Added ability to change module folder to swap parser for modded content
 - Hot reloading support for rendering, changes to the parser requires reloading the replay
-- Fixed map not including all geometry for the ground`);    
+- Fixed map not including all geometry for the ground (really only applies to R8E2)`);    
 
 export const Info = Macro(class Info extends MacroElement {
     public view = signal<Macro<typeof View> | undefined>(undefined);
