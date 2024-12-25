f005ccfed17c1725a386fd32df0fc1c3c65e7684
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-12-25 10:25:40 +0000

    chore: update RHU

--- Viewer/assets/@types/rhu/weak.d.ts
+++ Viewer/assets/@types/rhu/weak.d.ts
@@ -1,53 +1,18 @@
-interface Constructor {
-    new (...args: any[]): any;
-    prototype: any;
+export declare class WeakRefMap<K, V extends object> extends Map<K, V> {
+    set(key: K, value: V): any;
+    get(key: K): V | undefined;
+    values(): Generator<any, void, unknown>;
+    [Symbol.iterator](): IterableIterator<[key: K, value: V]>;
 }
-interface ReflectConstruct<Base extends Constructor, T> extends Constructor {
-    __reflect__(newTarget: any, args: any[]): T | undefined;
-    __constructor__(...args: any[]): void;
-    __args__(...args: any[]): ConstructorParameters<Base>;
-}
-export interface WeakRefMap<K, V> extends Map<K, V> {
-    prototype: WeakRefMap<K, V>;
-}
-interface WeakRefMapConstructor extends ReflectConstruct<MapConstructor, WeakRefMapConstructor> {
-    new (): WeakRefMap<any, any>;
-    new <K, V>(entries?: readonly (readonly [K, V])[] | null): WeakRefMap<K, V>;
-    readonly prototype: WeakRefMap<any, any>;
-}
-export interface WeakCollection<T extends object> extends WeakSet<T> {
-    prototype: WeakCollection<T>;
-    _collection: WeakRef<T>[];
-    __collection: WeakRef<T>[];
-    add(item: T): this;
-    delete(item: T): boolean;
-    add(...items: T[]): void;
-    delete(...items: T[]): void;
-    [Symbol.iterator](): IterableIterator<T>;
+export declare class WeakCollection<T extends object> extends WeakSet<T> {
+    private _collection;
+    private __collection;
+    add(...items: T[]): any;
+    delete(...items: T[]): any;
+    [Symbol.iterator](): Generator<T, void, unknown>;
     size(): number;
 }
-interface WeakCollectionConstructor extends ReflectConstruct<WeakSetConstructor, WeakCollectionConstructor> {
-    new <T extends object = object>(values?: readonly T[] | null): WeakCollection<T>;
-    readonly prototype: WeakCollection<object>;
+export declare class WeakCollectionMap<K, V extends object> extends Map<K, V> {
+    set(key: K, value: V): any;
+    add: (key: any, value: any) => any;
 }
-export interface WeakCollectionMap<K, V extends object> {
-    prototype: WeakCollectionMap<K, V>;
-    delete(key: K): boolean;
-    clear(): void;
-    set(key: K, value: V): WeakCollectionMap<K, V>;
-    add(key: K, value: V): WeakCollectionMap<K, V>;
-    get(key: K): WeakCollection<V> | undefined;
-    has(key: K): boolean;
-    values(): IterableIterator<WeakCollection<V>>;
-    entries(): IterableIterator<[key: K, value: WeakCollection<V>]>;
-    [Symbol.iterator](): IterableIterator<[key: K, value: WeakCollection<V>]>;
-}
-interface WeakCollectionMapConstructor extends ReflectConstruct<MapConstructor, WeakCollectionMapConstructor> {
-    new (): WeakCollectionMap<any, any>;
-    new <K, V extends object>(entries?: readonly (readonly [K, V])[] | null): WeakCollectionMap<K, V>;
-    readonly prototype: WeakCollectionMap<any, any>;
-}
-export declare const WeakRefMap: WeakRefMapConstructor;
-export declare const WeakCollection: WeakCollectionConstructor;
-export declare const WeakCollectionMap: WeakCollectionMapConstructor;
-export {};
--- Viewer/assets/js3party/rhu/weak.js
+++ Viewer/assets/js3party/rhu/weak.js
@@ -1,115 +1,45 @@
-function isConstructor(object) {
-    try {
-        Reflect.construct(String, [], object);
-    }
-    catch (e) {
-        return false;
-    }
-    return true;
-}
-function inherit(child, base) {
-    Object.setPrototypeOf(child.prototype, base.prototype);
-    Object.setPrototypeOf(child, base);
-}
-function reflectConstruct(base, name, constructor, argnames) {
-    if (!isConstructor(base))
-        throw new TypeError("'constructor' and 'base' must be object constructors.");
-    let args = argnames;
-    if (args === undefined) {
-        args = ["...args"];
-        const STRIP_COMMENTS = /((\/\/.*$)|(\/\*.*\*\/))/mg;
-        const funcString = constructor.toString().replace(STRIP_COMMENTS, "");
-        if (funcString.indexOf("function") === 0) {
-            const s = funcString.substring("function".length).trimStart();
-            args = s.substring(s.indexOf("(") + 1, s.indexOf(")"))
-                .split(",")
-                .map((a) => {
-                let clean = a.trim();
-                clean = clean.split(/[ =]/)[0];
-                return clean;
-            })
-                .filter((c) => c !== "");
-        }
-    }
-    let definition;
-    const argstr = args.join(",");
-    if (name === undefined)
-        name = constructor.name;
-    name.replace(/[ \t\r\n]/g, "");
-    if (name === "")
-        name = "__ReflectConstruct__";
-    const parts = name.split(".").filter(c => c !== "");
-    let evalStr = "{ let ";
-    for (let i = 0; i < parts.length - 1; ++i) {
-        const part = parts[i];
-        evalStr += `${part} = {}; ${part}.`;
-    }
-    evalStr += `${parts[parts.length - 1]} = function(${argstr}) { return definition.__reflect__.call(this, new.target, [${argstr}]); }; definition = ${parts.join(".")} }`;
-    eval(evalStr);
-    if (definition === undefined) {
-        console.warn("eval() call failed to create reflect constructor. Using fallback...");
-        definition = function (...args) {
-            return definition.__reflect__.call(this, new.target, args);
-        };
-    }
-    definition.__constructor__ = constructor;
-    definition.__args__ = function () {
-        return [];
-    };
-    definition.__reflect__ = function (newTarget, args = []) {
-        if (newTarget !== undefined) {
-            const obj = Reflect.construct(base, definition.__args__(...args), definition);
-            definition.__constructor__.call(obj, ...args);
-            return obj;
-        }
-        else
-            definition.__constructor__.call(this, ...args);
-    };
-    return definition;
-}
 const Map_set = Map.prototype.set;
 const Map_keys = Map.prototype.keys;
 const Map_get = Map.prototype.get;
 const Map_has = Map.prototype.has;
+const WeakSet_add = WeakSet.prototype.add;
+const WeakSet_delete = WeakSet.prototype.delete;
 const weakRefMapRegistry = new FinalizationRegistry(({ map, key }) => {
     map.delete(key);
 });
-export const WeakRefMap = reflectConstruct(Map, "WeakRefMap", function () {
-});
-WeakRefMap.prototype.set = function (key, value) {
-    weakRefMapRegistry.register(value, { map: this, key });
-    return Map_set.call(this, key, new WeakRef(value));
-};
-WeakRefMap.prototype.get = function (key) {
-    const raw = Map_get.call(this, key);
-    if (raw === undefined)
-        return undefined;
-    const value = raw.deref();
-    if (value === undefined)
-        return undefined;
-    return value;
-};
-WeakRefMap.prototype.values = function* () {
-    for (const key of Map_keys.call(this)) {
-        const value = Map_get.call(this, key).deref();
-        if (value !== undefined)
-            yield value;
-        else
-            this.delete(key);
+export class WeakRefMap extends Map {
+    set(key, value) {
+        weakRefMapRegistry.register(value, { map: this, key });
+        return Map_set.call(this, key, new WeakRef(value));
     }
-};
-WeakRefMap.prototype[Symbol.iterator] = function* () {
-    for (const key of Map_keys.call(this)) {
-        const value = Map_get.call(this, key).deref();
-        if (value !== undefined)
-            yield [key, value];
-        else
-            this.delete(key);
+    get(key) {
+        const raw = Map_get.call(this, key);
+        if (raw === undefined)
+            return undefined;
+        const value = raw.deref();
+        if (value === undefined)
+            return undefined;
+        return value;
     }
-};
-inherit(WeakRefMap, Map);
-const WeakSet_add = WeakSet.prototype.add;
-const WeakSet_delete = WeakSet.prototype.delete;
+    *values() {
+        for (const key of Map_keys.call(this)) {
+            const value = Map_get.call(this, key).deref();
+            if (value !== undefined)
+                yield value;
+            else
+                this.delete(key);
+        }
+    }
+    *[Symbol.iterator]() {
+        for (const key of Map_keys.call(this)) {
+            const value = Map_get.call(this, key).deref();
+            if (value !== undefined)
+                yield [key, value];
+            else
+                this.delete(key);
+        }
+    }
+}
 const weakCollectionRegistry = new FinalizationRegistry((map) => {
     const collection = map._collection;
     map._collection = map.__collection;
@@ -120,82 +50,86 @@ const weakCollectionRegistry = new FinalizationRegistry((map) => {
     map.__collection = collection;
     map.__collection.length = 0;
 });
-export const WeakCollection = reflectConstruct(WeakSet, "WeakCollection", function () {
-    this._collection = [];
-    this.__collection = [];
-});
-WeakCollection.prototype.add = function (...items) {
-    if (items.length === 1) {
-        this._collection.push(new WeakRef(items[0]));
-        weakCollectionRegistry.register(items[0], this);
-        return WeakSet_add.call(this, items[0]);
+export class WeakCollection extends WeakSet {
+    constructor() {
+        super(...arguments);
+        this._collection = [];
+        this.__collection = [];
     }
-    for (const item of items) {
-        if (!this.has(item)) {
-            this._collection.push(new WeakRef(item));
-            WeakSet_add.call(this, item);
-            weakCollectionRegistry.register(item, this);
+    add(...items) {
+        if (items.length === 1) {
+            this._collection.push(new WeakRef(items[0]));
+            weakCollectionRegistry.register(items[0], this);
+            return WeakSet_add.call(this, items[0]);
+        }
+        for (const item of items) {
+            if (!this.has(item)) {
+                this._collection.push(new WeakRef(item));
+                WeakSet_add.call(this, item);
+                weakCollectionRegistry.register(item, this);
+            }
         }
     }
-};
-WeakCollection.prototype.delete = function (...items) {
-    if (items.length === 1) {
+    delete(...items) {
+        if (items.length === 1) {
+            this._collection = this._collection.filter((ref) => {
+                const item = ref.deref();
+                return item !== undefined && !items.includes(item);
+            });
+            return WeakSet_delete.call(this, items[0]);
+        }
+        for (const item of items)
+            WeakSet_delete.call(this, item);
         this._collection = this._collection.filter((ref) => {
             const item = ref.deref();
             return item !== undefined && !items.includes(item);
         });
-        return WeakSet_delete.call(this, items[0]);
     }
-    for (const item of items)
-        WeakSet_delete.call(this, item);
-    this._collection = this._collection.filter((ref) => {
-        const item = ref.deref();
-        return item !== undefined && !items.includes(item);
-    });
-};
-WeakCollection.prototype[Symbol.iterator] = function* () {
-    const collection = this._collection;
-    this._collection = this.__collection;
-    for (const ref of collection) {
-        const item = ref.deref();
-        if (item !== undefined) {
-            this._collection.push(ref);
-            yield item;
+    *[Symbol.iterator]() {
+        const collection = this._collection;
+        this._collection = this.__collection;
+        for (const ref of collection) {
+            const item = ref.deref();
+            if (item !== undefined) {
+                this._collection.push(ref);
+                yield item;
+            }
         }
+        this.__collection = collection;
+        this.__collection.length = 0;
     }
-    this.__collection = collection;
-    this.__collection.length = 0;
-};
-WeakCollection.prototype.size = function () {
-    let count = 0;
-    const collection = this._collection;
-    this._collection = this.__collection;
-    for (const ref of collection) {
-        const item = ref.deref();
-        if (item !== undefined) {
-            this._collection.push(ref);
-            ++count;
+    size() {
+        let count = 0;
+        const collection = this._collection;
+        this._collection = this.__collection;
+        for (const ref of collection) {
+            const item = ref.deref();
+            if (item !== undefined) {
+                this._collection.push(ref);
+                ++count;
+            }
         }
+        this.__collection = collection;
+        this.__collection.length = 0;
+        return count;
     }
-    this.__collection = collection;
-    this.__collection.length = 0;
-    return count;
-};
-inherit(WeakCollection, WeakSet);
+}
 const weakCollectionMapRegistry = new FinalizationRegistry(({ map, key, collection }) => {
     if (collection.size() === 0)
         map.delete(key);
 });
-export const WeakCollectionMap = reflectConstruct(Map, "WeakCollectionMap", function () {
-});
-WeakCollectionMap.prototype.set = function (key, value) {
-    if (!Map_has.call(this, key)) {
-        Map_set.call(this, key, new WeakCollection());
+export class WeakCollectionMap extends Map {
+    constructor() {
+        super(...arguments);
+        this.add = WeakCollectionMap.prototype.set;
     }
-    const collection = Map_get.call(this, key);
-    collection.add(value);
-    weakCollectionMapRegistry.register(value, { map: this, key, collection });
-    return Map_set.call(this, key, collection);
-};
-WeakCollectionMap.prototype.add = WeakCollectionMap.prototype.set;
-inherit(WeakCollectionMap, Map);
+    set(key, value) {
+        if (!Map_has.call(this, key)) {
+            Map_set.call(this, key, new WeakCollection());
+        }
+        const collection = Map_get.call(this, key);
+        collection.add(value);
+        weakCollectionMapRegistry.register(value, { map: this, key, collection });
+        return Map_set.call(this, key, collection);
+    }
+}
